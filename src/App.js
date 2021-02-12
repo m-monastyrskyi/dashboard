9dbf2c0d4449feae2268758db6c98ffc5b81169e
Mykola Monastyrskyi <kolyan4ik@gmail.com>
2021-02-12 20:04:39 +0100

    installed axios, implemented data fetching with axios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [error, setError] = useState(null);
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        const url = 'https://my-json-server.typicode.com/karolkproexe/jsonplaceholderdb/data';
+        setLoading(true);
+        axios.get(url)
+            .then(res => {
+                console.log(res);
+                setLoading(false);
+                res.status === 200 && setUsers(res.data);
+            })
+            .catch(error => {
+                console.log(error);
+                setError(error);
+                setLoading(false);
+            });
+
+    }, []);
+
+    if ( loading ) {
+        return <h1>Loading...</h1>;
+    }
+
+    if ( error ) {
+        return <>
+            <h1>Ups... Something went wrong...</h1>
+            <pre>{error.message}</pre>
+        </>;
+    }
+
+    return (<>
+            <h1>Users:</h1>
+            <ul>
+                {
+                    users.map(user => <li key={user.id}>{user.name} - {user.email}</li>)
+                }
+            </ul>
+
+        </>
+    );
+};
 
 export default App;
