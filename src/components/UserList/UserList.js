30104f52dee56c763d19140c46c6c29a9c80e54b
Mykola Monastyrskyi <kolyan4ik@gmail.com>
2021-02-13 08:23:30 +0100

    implemented table column sorters

--- src/components/UserList/UserList.js
+++ src/components/UserList/UserList.js
@@ -1,18 +1,26 @@
-import {  Table } from 'antd';
+import { Table } from 'antd';
 import CustomButton from '../CustomButton';
 
 const UserList = ({ users }) => {
     const data = users.map(user => ({ key: user.id, ...user }));
     const columns = [
-        { title: 'ID', dataIndex: 'id' },
+        {
+            title: 'ID', dataIndex: 'id',
+            sorter: (a, b) => a.id - b.id
+        },
         {
             title: 'Name', dataIndex: 'name',
+            sorter: (a, b) => a.name.localeCompare(b.name),
             render: text => <a>{text}</a>,
         },
-        { title: 'Username', dataIndex: 'username' },
+        {
+            title: 'Username', dataIndex: 'username',
+            sorter: (a, b) => a.username.localeCompare(b.username)
+        },
         {
             title: 'City',
             key: 'city',
+            sorter: (a, b) => a.address.city.localeCompare(b.address.city),
             render: (_, record) => record.address.city
         },
         {
